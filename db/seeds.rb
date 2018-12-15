40ca690c6579796c2be3865bc0b853f39889bdd8
Jose Medina <fermed28@gmail.com>
2018-12-15 16:36:21 -0500

    generate Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,29 @@ Category.find_or_create_by(name:'Restaurants')
 Category.find_or_create_by(name:'Car')
 Category.find_or_create_by(name:'Travel')
 Category.find_or_create_by(name:'Fun')
+
+type_of_expense = ["purchase", "withdrawal", "transfer","payment"]
+
+def rand_in_range(from, to)
+    rand * (to - from) + from
+end
+
+def rand_time(from, to=Time.now)
+    Time.at(rand_in_range(3.years.ago.to_f, Time.now.to_f)).to_date
+end
+
+
+def rand_category
+    Category.order("Random()").pluck(:id).first
+end
+
+def rand_amount
+    rand * (120000) + 8000
+end
+
+if Category.all.count < 1000
+    1000.times do 
+        category = rand_category
+        Expense.create(type_of_expense: type_of_expense.sample, date: rand_time(3.years.ago), concept: Faker::Commerce.product_name, amount: rand_amount, category_id: category)
+    end
+end
